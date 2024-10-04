95f7111b9c8a88d6a71d079390c242f8014c03b5
Emanuel Francisco Gomez <emanuel.francisco.gomez@mendix.com>
2024-10-04 14:45:34 +0200

    End of section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,5 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -10,36 +9,32 @@ namespace API.Controllers
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")] //api/activities/{id}
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query { Id = id });
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
         }
 
         [HttpPost] //api/activities
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            await Mediator.Send(new Create.Command { Activity = activity });
-
-            return Ok();
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")] //api/activities/{id}
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            await Mediator.Send(new Edit.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
 
         [HttpDelete("{id}")] //api/activities/{id}
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            await Mediator.Send(new Delete.Command { Id = id });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
 
     }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,22 +1,24 @@
-import { Fragment } from 'react'
 import { Container } from 'semantic-ui-react'
 import NavBar from './NavBar'
 import { observer } from 'mobx-react-lite'
 import { Outlet, useLocation } from 'react-router-dom'
 import HomePage from '../../features/home/HomePage'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
+
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       {location.pathname === '/' ? <HomePage /> : (
-        <Fragment>
+        <>
           <NavBar />
           <Container style={{ marginTop: '7em' }}>
             <Outlet />
           </Container>
-        </Fragment>
+        </>
       )}
     </>
   )
